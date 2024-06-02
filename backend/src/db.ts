5230b50d6391e9d581b03d0306792b0301be7d8d
wizard <wizardmakes@gmail.com>
2024-06-02 08:36:10 +0530

    prisma init with postgress

---
+++ backend/src/db.ts
@@ -0,0 +1,24 @@
+import { Client } from "pg";
+
+const connectionString =
+  "postgresql://postgres:mysecretpassword@localhost:5432/postgres";
+
+const client = new Client({
+  connectionString: connectionString,
+});
+
+client.connect((err) => {
+  if (err) {
+    console.log("err", err.stack);
+  } else {
+    console.log("connected");
+  }
+});
+
+client.query("SELECT NOW()", (err, res) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(res.rows[0]);
+  }
+});
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,33 @@
-const x: number = 1;
-console.log(x);
+import { log } from "console";
+import express from "express";
+
+const app: express.Application = express();
+const port: number = 3000;
+
+app.post("/api/v1/user/signup", (req, res) => {
+  console.log("hii");
+});
+
+app.post("/api/v1/user/signin", (req, res) => {
+  console.log("hii1");
+});
+
+app.post("/api/v1/blog", (req, res) => {
+  console.log("hi2");
+});
+
+app.put("/api/v1/blog", (req, res) => {
+  console.log("hi3");
+});
+
+app.get("/api/v1/blog/:id", (req, res) => {
+  console.log("hi4");
+});
+
+app.get("/api/v1/blog/bulk", (req, res) => {
+  console.log("hi5");
+});
+
+app.listen(port, () => {
+  console.log("running");
+});
